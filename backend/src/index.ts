a1c2a44af1c55eac7000d17d3020f77e194034a9
ShirshakShahi <royalshirshal000@gmail.com>
2024-01-08 23:38:12 +0545

    added be

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { Request, Response } from "express";
+import connectDB from "./db/connect";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ msg: "hiiiiiiiieeeeeeeeeeeeeeeee" });
+});
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`server listening on port http://localhost:${port}`)
+    );
+  } catch (error) {
+    console.log(`Error occured as ${error.message}`);
+  }
+};
+
+start();
