424ff8798043e633f1f7f989c7cb4a234c44da39
Shirshak <103033347+ShirshakShahi@users.noreply.github.com>
2024-01-11 15:47:20 +0545

    delete controller.

--- backend/src/controllers/post.controller.ts
+++ backend/src/controllers/post.controller.ts
@@ -3,7 +3,6 @@ import { Post } from "../models/post.model";
 export const addPost = async (req, res) => {
   try {
     const { title, content } = req.body;
-
     const newPost = new Post({
       title,
       content,
@@ -14,13 +13,12 @@ export const addPost = async (req, res) => {
     res.status(200).json({ newPost });
   } catch (error) {
     console.log("error ", error);
-    res.json(500).json({ msg: "Internal Server Error!!!" });
+    res.status(500).json({ msg: "Internal Server Error!!!" });
   }
 };
 
 export const updatePost = async (req, res) => {
   const { postId } = req.params;
-
   try {
     const post = await Post.findById(postId);
     if (!post) {
@@ -39,6 +37,29 @@ export const updatePost = async (req, res) => {
     const updatedPost = await post.save();
 
     res.status(200).json({ updatedPost });
+  } catch (err) {
+    console.log("errorrrrr", err);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { postId } = req.params;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      res.status(404).json({ msg: `No post with id ${postId} found!!!` });
+      return;
+    }
+
+    if (!req.user._id.equals(post.user)) {
+      res.status(401).json({ msg: "Invalid Credentials" });
+      return;
+    }
+
+    const deletedPost = await post.deleteOne();
+
+    res.status(200).json({ deletedPost });
   } catch (err) {
     console.log("errorrrrr", err);
     return res.status(500).json({ msg: "Internal Server Error" });
